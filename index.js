18b93d3eb360c551faf86bdf1d06d3d06b9ecc75
Diego Alvial <blazi.alv@gmail.com>
2023-05-18 20:06:35 -0400

    docker para linux y solo requiere iniciar una vez

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const { Player } = require("discord-player");
 dotenv.config();
 const TOKEN = process.env.TOKEN;
 
-const LOAD_SLASH = process.argv[2] == "load";
-
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
@@ -54,43 +52,39 @@ const slashFiles = fs
 for (const file of slashFiles) {
   const slashcmd = require(`./slash/${file}`);
   client.slashcommands.set(slashcmd.data.name, slashcmd);
-  if (LOAD_SLASH) commands.push(slashcmd.data.toJSON());
+  commands.push(slashcmd.data.toJSON());
 }
 
-if (LOAD_SLASH) {
-  const rest = new REST({ version: "9" }).setToken(TOKEN);
-  console.log("Deploying slash commands");
-  rest
-    .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-      body: commands,
-    })
-    .then(() => {
-      console.log("Successfully loaded");
-      process.exit(0);
-    })
-    .catch((err) => {
-      if (err) {
-        console.log(err);
-        process.exit(1);
-      }
-    });
-} else {
-  client.on("ready", async () => {
-    await client.player.extractors.loadDefault();
-    console.log(`Logged in as ${client.user.tag}`);
-
-  });
-  client.on("interactionCreate", (interaction) => {
-    async function handleCommand() {
-      if (!interaction.isCommand()) return;
-
-      const slashcmd = client.slashcommands.get(interaction.commandName);
-      if (!slashcmd) interaction.reply("No es un comando de slash válido");
-
-      //await interaction.deferReply();
-      await slashcmd.run({ client, interaction });
+const rest = new REST({ version: "9" }).setToken(TOKEN);
+console.log("Deploying slash commands");
+rest
+  .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+    body: commands,
+  })
+  .then(() => {
+    console.log("Successfully loaded");
+  })
+  .catch((err) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
     }
-    handleCommand();
   });
-  client.login(TOKEN);
-}
+client.on("ready", async () => {
+  await client.player.extractors.loadDefault();
+  console.log(`Logged in as ${client.user.tag}`);
+
+});
+client.on("interactionCreate", (interaction) => {
+  async function handleCommand() {
+    if (!interaction.isCommand()) return;
+
+    const slashcmd = client.slashcommands.get(interaction.commandName);
+    if (!slashcmd) interaction.reply("No es un comando de slash válido");
+
+    //await interaction.deferReply();
+    await slashcmd.run({ client, interaction });
+  }
+  handleCommand();
+});
+client.login(TOKEN);
